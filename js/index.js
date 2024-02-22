bc6d487617b92fc8edf695b6a0dc38d76f59427a
Hai Tran <tranvanhai240x@gmail.com>
2024-02-22 16:07:06 +0700

    fix bug homepage round 2

--- js/index.js
+++ js/index.js
@@ -31,6 +31,21 @@ $(".slider-mobile").slick({
     },
   ],
 });
+function openTab(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
 
 // Gsap animation - card scroll
 $(function () {
@@ -76,13 +91,13 @@ $("a").on("click", function (event) {
 });
 
 // Sign up
-$(".slider-signup").slick({
-  dots: true,
-  arrows: true,
-  slidesToShow: 1,
-  autoplay: true,
-  autoplaySpeed: 5000,
-});
+// $(".slider-signup").slick({
+//   dots: true,
+//   arrows: true,
+//   slidesToShow: 1,
+//   autoplay: true,
+//   autoplaySpeed: 5000,
+// });
 
 // Forgot password
 setTimeout(() => {
